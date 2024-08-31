fb134e8158a57a991b05e3cad9ea35b8a9f6876d
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-08-31 17:57:39 +0600

    array

---
+++ 11_array.js
@@ -0,0 +1,17 @@
+/**
+ * 1. Declare an array
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array
+ */
+
+let fruits = ['apple', 'banana', 'grapes', 'mango', 'strawberry'];
+console.log(fruits);
+
+console.log(fruits[3]);
+
+fruits[2]='jambura';
+console.log(fruits);
+
+
