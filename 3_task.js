2336664376099f4a8ccb1ba522e56bcb17cf2bb4
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-08-31 14:54:25 +0600

    task3 remainder

---
+++ 3_task.js
@@ -0,0 +1,13 @@
+/**
+ * Task-3
+Your task is to divide the given number by 5 and show the remainder as the output.
+
+Input:
+The first line of the input contains the number.
+
+Output:
+Print the remainder.
+ */
+
+let num = 194;
+console.log(num%5);
