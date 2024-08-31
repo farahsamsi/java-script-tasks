c34b5e081210980d4542faeaf4765643bed567f3
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-08-31 18:14:44 +0600

    includes conditional

---
+++ 13_includes.js
@@ -0,0 +1,22 @@
+/**
+ * 3. Checking Array Membership with ‘includes’
+Instructions:
+
+Create an array of books containing different book.
+
+Use the includes method to check if the array contains a javascript book.
+
+Print a message to the console indicating whether the element is present in the array or not.
+ */
+
+let books = ['amazon', 'geology', 'javascript', 'html'];
+
+if(books.includes('javascript')){
+    console.log('yes');
+}
+// if(books.includes('pgo')){
+//     console.log('yes');
+// }
+else{
+    console.log('no');
+}
