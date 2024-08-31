7503925efc415ceeb65a03c84db1b5117791d3b3
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-08-31 18:06:25 +0600

    push pop

---
+++ 12_addelement.js
@@ -0,0 +1,21 @@
+/**
+ * 2. Add or remove elements
+Declare an array of 3 tourist destinations
+Add a new tourist destination to your tourist array
+Add two more to your array
+Remove the last tourist destination you have added
+display the final array as output
+ */
+
+let destination = ['rangpur', 'dhaka', 'rangamati'];
+console.log(destination);
+
+destination.push('jessore');
+console.log(destination);
+
+destination.push('netrokona', 'khulna');
+console.log(destination);
+
+destination.pop();
+console.log(destination);
+
