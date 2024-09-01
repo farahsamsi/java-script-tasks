4fb4c9e354ceeb0fcd738655e9558d9fc79e4cf4
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-09-01 11:51:38 +0600

    array concat

---
+++ 15_combining.js
@@ -0,0 +1,18 @@
+/**
+ * 5. Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+ */
+
+let first= [34, 323, 55, 33, 56];
+let second = [34, 98, 90, 23, 895];
+
+console.log('first:', first);
+console.log('second:', second);
+
+let merged= first.concat(second);
+console.log('merged:', merged);
+
