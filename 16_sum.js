835208ecdfa166a6b2025fcded032f8d46cf6798
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-09-02 06:12:23 +0600

    sum of consecutive numbers using while loop

--- 16_sum.js
+++ 16_sum.js
@@ -1,7 +1,17 @@
+
+
 let sum=0;
 
-for(let i=11 ; i<=20 ; i++){
+// for(let i=11 ; i<=20 ; i++){
+//     console.log(i);
+//     sum=sum+i;
+// }
+// console.log('sum of numbers 11 to 20 is:',sum);
+
+let i=11;
+while(i<=20){
     console.log(i);
     sum=sum+i;
+    i++;
 }
 console.log('sum of numbers 11 to 20 is:',sum);
