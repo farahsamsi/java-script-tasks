c484bc12a10669f99404b28185f050ce6f5f2549
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-09-03 13:27:28 +0600

    multiplication table of 9

---
+++ 20_mutiplication.js
@@ -0,0 +1,19 @@
+/***
+
+Generate a multiplication table for number 9
+
+ */
+
+let product = 0;
+// for (let i = 1; i <= 10; i++) {
+//     product = 9 * i;
+//     console.log('9 multiplied by', i, 'is', product);
+// }
+
+
+let i=1;
+while(i<=10){
+    product = 9 * i;
+    console.log('9 multiplied by', i, 'is', product);
+    i++;
+}
